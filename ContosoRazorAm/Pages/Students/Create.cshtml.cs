a01dfb76804d95ebd44775a6367ff2106ccdc634
Derek Peacock <derek_peacock@yahoo.com>
2022-10-27 11:59:29 +0100

    Added db intitialiswer

---
+++ ContosoRazorAm/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoRazorAm.Data;
+using ContosoRazorAm.Models;
+
+namespace ContosoRazorAm.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoRazorAm.Data.ApplicationDbContext _context;
+
+        public CreateModel(ContosoRazorAm.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ContosoRazorAm/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoRazorAm.Data;
+using ContosoRazorAm.Models;
+
+namespace ContosoRazorAm.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoRazorAm.Data.ApplicationDbContext _context;
+
+        public IndexModel(ContosoRazorAm.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Students != null)
+            {
+                Student = await _context.Students.ToListAsync();
+            }
+        }
+    }
+}
